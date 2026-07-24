b4ffaff1b6ebbf8f3810c43fc75706f234b4c2ad
ashfaqahmad134-wq <ashfaqahmad134@gmail.com>
2026-07-24 17:38:44 +0500

    LinkedIn tab fragment

---
+++ src/main/kotlin/com/autoviwer/watcher/ui/fragments/LinkedInFragment.kt
@@ -0,0 +1,31 @@
+package com.autoviwer.watcher.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.autoviwer.watcher.databinding.FragmentPlatformBinding
+
+class LinkedInFragment : Fragment() {
+    
+    private lateinit var binding: FragmentPlatformBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentPlatformBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+        binding.apply {
+            platformName.text = "LinkedIn"
+            // Setup similar to YouTube
+        }
+    }
+}
